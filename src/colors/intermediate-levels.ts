9114ee92a6d4bcc6e974a41795140353fc2ab0fa
DeeqaJamalini <deeqa.j11@gmail.com>
2023-12-09 17:15:04 +0000

    added new levels

---
+++ src/colors/intermediate-levels.ts
@@ -0,0 +1,194 @@
+import { Level } from "../types/level";
+
+
+
+export const intermediateLevelOne: Level = {
+    matcherButton: {
+      "matcher-button": "#1fd5e0",
+    },
+    colorButtons: [
+      { color: "#1fc8cf", matcher: false },
+      { color: "#1fb0c6", matcher: false },
+      { color: "#1fa8bf", matcher: false },
+      { color: "#1fa0b8", matcher: false },
+      { color: "#1fd5e0", matcher: true },
+      { color: "#1f98b1", matcher: false },
+      { color: "#1f90aa", matcher: false },
+      { color: "#1f88a3", matcher: false },
+      { color: "#1f809c", matcher: false },
+      { color: "#1f7895", matcher: false },
+      { color: "#5ae0e8", matcher: false },
+      { color: "#1f6490", matcher: false },
+      { color: "#1f5c89", matcher: false },
+  
+    ],
+  };
+
+
+  
+export const intermediateLevelTwo: Level = {
+    matcherButton: {
+      "matcher-button": "#1fd5e0",
+    },
+    colorButtons: [
+      { color: "#1fc8cf", matcher: false },
+      { color: "#1fb0c6", matcher: false },
+      { color: "#1fa8bf", matcher: false },
+      { color: "#1fa0b8", matcher: false },
+      { color: "#1fd5e0", matcher: true },
+      { color: "#1f98b1", matcher: false },
+      { color: "#1f90aa", matcher: false },
+      { color: "#1f88a3", matcher: false },
+      { color: "#1f809c", matcher: false },
+      { color: "#1f7895", matcher: false },
+      { color: "#5ae0e8", matcher: false },
+      { color: "#1f6490", matcher: false },
+      { color: "#1f5c89", matcher: false },
+  
+    ],
+  };
+
+
+  
+export const intermediateLevelThree: Level = {
+    matcherButton: {
+      "matcher-button": "#1fd5e0",
+    },
+    colorButtons: [
+      { color: "#1fc8cf", matcher: false },
+      { color: "#1fb0c6", matcher: false },
+      { color: "#1fa8bf", matcher: false },
+      { color: "#1fa0b8", matcher: false },
+      { color: "#1fd5e0", matcher: true },
+      { color: "#1f98b1", matcher: false },
+      { color: "#1f90aa", matcher: false },
+      { color: "#1f88a3", matcher: false },
+      { color: "#1f809c", matcher: false },
+      { color: "#1f7895", matcher: false },
+      { color: "#5ae0e8", matcher: false },
+      { color: "#1f6490", matcher: false },
+      { color: "#1f5c89", matcher: false },
+  
+    ],
+  };
+  
+
+  
+export const intermediateLevelFour: Level = {
+    matcherButton: {
+      "matcher-button": "#1fd5e0",
+    },
+    colorButtons: [
+      { color: "#1fc8cf", matcher: false },
+      { color: "#1fb0c6", matcher: false },
+      { color: "#1fa8bf", matcher: false },
+      { color: "#1fa0b8", matcher: false },
+      { color: "#1fd5e0", matcher: true },
+      { color: "#1f98b1", matcher: false },
+      { color: "#1f90aa", matcher: false },
+      { color: "#1f88a3", matcher: false },
+      { color: "#1f809c", matcher: false },
+      { color: "#1f7895", matcher: false },
+      { color: "#5ae0e8", matcher: false },
+      { color: "#1f6490", matcher: false },
+      { color: "#1f5c89", matcher: false },
+  
+    ],
+  };
+  
+
+  
+export const intermediateLevelFive: Level = {
+    matcherButton: {
+      "matcher-button": "#1fd5e0",
+    },
+    colorButtons: [
+      { color: "#1fc8cf", matcher: false },
+      { color: "#1fb0c6", matcher: false },
+      { color: "#1fa8bf", matcher: false },
+      { color: "#1fa0b8", matcher: false },
+      { color: "#1fd5e0", matcher: true },
+      { color: "#1f98b1", matcher: false },
+      { color: "#1f90aa", matcher: false },
+      { color: "#1f88a3", matcher: false },
+      { color: "#1f809c", matcher: false },
+      { color: "#1f7895", matcher: false },
+      { color: "#5ae0e8", matcher: false },
+      { color: "#1f6490", matcher: false },
+      { color: "#1f5c89", matcher: false },
+  
+    ],
+  };
+  
+
+  
+export const intermediateLevelSix: Level = {
+    matcherButton: {
+      "matcher-button": "#1fd5e0",
+    },
+    colorButtons: [
+      { color: "#1fc8cf", matcher: false },
+      { color: "#1fb0c6", matcher: false },
+      { color: "#1fa8bf", matcher: false },
+      { color: "#1fa0b8", matcher: false },
+      { color: "#1fd5e0", matcher: true },
+      { color: "#1f98b1", matcher: false },
+      { color: "#1f90aa", matcher: false },
+      { color: "#1f88a3", matcher: false },
+      { color: "#1f809c", matcher: false },
+      { color: "#1f7895", matcher: false },
+      { color: "#5ae0e8", matcher: false },
+      { color: "#1f6490", matcher: false },
+      { color: "#1f5c89", matcher: false },
+  
+    ],
+  };
+  
+
+  
+export const intermediateLevelSeven: Level = {
+    matcherButton: {
+      "matcher-button": "#1fd5e0",
+    },
+    colorButtons: [
+      { color: "#1fc8cf", matcher: false },
+      { color: "#1fb0c6", matcher: false },
+      { color: "#1fa8bf", matcher: false },
+      { color: "#1fa0b8", matcher: false },
+      { color: "#1fd5e0", matcher: true },
+      { color: "#1f98b1", matcher: false },
+      { color: "#1f90aa", matcher: false },
+      { color: "#1f88a3", matcher: false },
+      { color: "#1f809c", matcher: false },
+      { color: "#1f7895", matcher: false },
+      { color: "#5ae0e8", matcher: false },
+      { color: "#1f6490", matcher: false },
+      { color: "#1f5c89", matcher: false },
+  
+    ],
+  };
+  
+
+  
+export const intermediateLevelEight: Level = {
+    matcherButton: {
+      "matcher-button": "#1fd5e0",
+    },
+    colorButtons: [
+      { color: "#1fc8cf", matcher: false },
+      { color: "#1fb0c6", matcher: false },
+      { color: "#1fa8bf", matcher: false },
+      { color: "#1fa0b8", matcher: false },
+      { color: "#1fd5e0", matcher: true },
+      { color: "#1f98b1", matcher: false },
+      { color: "#1f90aa", matcher: false },
+      { color: "#1f88a3", matcher: false },
+      { color: "#1f809c", matcher: false },
+      { color: "#1f7895", matcher: false },
+      { color: "#5ae0e8", matcher: false },
+      { color: "#1f6490", matcher: false },
+      { color: "#1f5c89", matcher: false },
+  
+    ],
+  };
+  
--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,103 @@
 import "./styles/main.scss";
-import { allBeginnerLevels } from "./colors/all-levels";
+import {
+  allBeginnerLevels,
+  allIntermediateLevels,
+  allExpertLevels,
+} from "./colors/all-levels";
 import confetti, { Options } from "canvas-confetti";
 
+let level = 0;
+let score = 0;
+let isClickable = false;
+let isIntermediateLevel = false;
+let isExpertLevel = false;
+let isLevelSelected = false; // New flag to track if a level is selected
+let levelData = allBeginnerLevels[level];
+let confettiInstance: any = null;
+
+let timeoutId: number;
 const buttons = Array.from(document.querySelectorAll(".button"));
 const matcher = document.querySelector<HTMLButtonElement>("#matcher");
 const gameButton = document.querySelector<HTMLButtonElement>("#game-button");
+const beginnerButton =
+  document.querySelector<HTMLButtonElement>("#beginner-button");
+const intermediateButton = document.querySelector<HTMLButtonElement>(
+  "#intermediate-button"
+);
+const expertButton =
+  document.querySelector<HTMLButtonElement>("#expert-button");
 const instructions =
   document.querySelector<HTMLParagraphElement>("#instructions");
 const scoreCounter =
   document.querySelector<HTMLParagraphElement>("#score-counter");
+const currentLevelDisplay =
+  document.querySelector<HTMLParagraphElement>("#current-level");
 const correctSound = new Audio(
   "./src/sounds/mixkit-ethereal-fairy-win-sound-2019.wav"
 );
+const completedLevelSound = new Audio(
+  "./src/sounds/mixkit-casting-long-fairy-magic-spell-875.wav"
+);
 const clickSound = new Audio("./src/sounds/mixkit-hard-pop-click-2364.wav");
-const clickSoundSpeed = 2.5;
+const clickSoundSpeed = 3;
 clickSound.playbackRate = clickSoundSpeed;
+let levelType = "";
+const originalButtonColors = buttons.map((button) => {
+  return (button as HTMLButtonElement).style.backgroundColor;
+});
 
 if (
   !matcher ||
   !gameButton ||
+  !beginnerButton ||
+  !intermediateButton ||
+  !expertButton ||
   !instructions ||
   !scoreCounter ||
+  !currentLevelDisplay ||
   buttons.length !== 13
 ) {
   throw new Error("Something is wrong with my HTML elements");
 }
 
-let level = 0;
-let score = 0;
-let isClickable = false;
-let levelData = allBeginnerLevels[level];
+const clearColorButtons = () => {
+  // Reset the buttons to their original colors
+  buttons.forEach((button, index) => {
+    (button as HTMLButtonElement).style.backgroundColor =
+      originalButtonColors[index];
+  });
+};
 
+const stopConfetti = () => {
+  if (confettiInstance) {
+    confettiInstance.reset();
+  }
+};
+
+const clearMatcherButton = () => {
+  (matcher as HTMLButtonElement).style.backgroundColor = "#FFFFFF";
+};
 const game = () => {
-  levelData = allBeginnerLevels[level];
+  if (isExpertLevel) {
+    levelData = allExpertLevels[level];
+    levelType = "Expert";
+  } else if (isIntermediateLevel) {
+    levelData = allIntermediateLevels[level];
+    levelType = "Intermediate";
+  } else {
+    levelData = allBeginnerLevels[level];
+    levelType = "Beginner";
+  }
   instructions.textContent = "Click the bubble that matches the color above";
   matcher.style.backgroundColor = levelData.matcherButton["matcher-button"];
+
   buttons.forEach((button, index) => {
     (button as HTMLButtonElement).style.backgroundColor =
       levelData.colorButtons[index].color;
   });
   isClickable = true;
+
+  currentLevelDisplay.textContent = `Playing ${levelType} level`;
 };
 
 const updateScore = () => {
@@ -51,7 +109,7 @@ const validateAnswer = (buttonIndex: number) => {
     return;
   }
 
-  const isMatch = allBeginnerLevels[level].colorButtons[buttonIndex].matcher;
+  const isMatch = levelData.colorButtons[buttonIndex].matcher;
 
   if (isMatch) {
     instructions.textContent = "Correct! You matched the bubble!";
@@ -71,11 +129,13 @@ const validateAnswer = (buttonIndex: number) => {
     level++;
     isClickable = false;
     if (level < allBeginnerLevels.length) {
-      setTimeout(() => {
+      timeoutId = setTimeout(() => {
         gameButton.textContent = "Restart";
         game();
       }, 2500);
     } else {
+      //test if you need to add a timeout to this
+      completedLevelSound.play();
       instructions.textContent = "Congratulations! You completed all levels.";
     }
   } else {
@@ -92,32 +152,75 @@ const validateAnswer = (buttonIndex: number) => {
 };
 
 const toggleGame = () => {
-  if (level === 0) {
-    startGame();
+  if (!isIntermediateLevel && !isExpertLevel && !isLevelSelected) {
+    instructions.textContent = "Please select a level first.";
   } else {
-    restartGame();
+    if (level === 0) {
+      startGame();
+    } else {
+      restartGame();
+    }
   }
 };
 
 const restartGame = () => {
   level = 0;
   score = 0;
+  isClickable = false;
   updateScore();
   gameButton.textContent = "Start";
   game();
 };
 
 const startGame = () => {
-  level = 0;
-  score = 0;
   updateScore();
   isClickable = true;
   game();
 };
 
+beginnerButton.addEventListener("click", () => {
+  isClickable = false;
+  clearTimeout(timeoutId);
+  isIntermediateLevel = false;
+  isExpertLevel = false;
+  isLevelSelected = true;
+  instructions.textContent =
+    "You selected the Beginner level. Click 'Start' to begin.";
+  clearColorButtons();
+  clearMatcherButton();
+  gameButton.textContent = "Start";
+  stopConfetti();
+});
+
+intermediateButton.addEventListener("click", () => {
+  isClickable = false;
+  clearTimeout(timeoutId);
+
+  isIntermediateLevel = true;
+  isExpertLevel = false;
+  isLevelSelected = true;
+  instructions.textContent =
+    "You selected the Intermediate level. Click 'Start' to begin.";
+  clearColorButtons();
+  clearMatcherButton();
+  gameButton.textContent = "Start";
+});
+
+expertButton.addEventListener("click", () => {
+  isClickable = false;
+  clearTimeout(timeoutId);
+  isIntermediateLevel = false;
+  isExpertLevel = true;
+  isLevelSelected = true;
+  instructions.textContent =
+    "You selected the Expert level. Click 'Start' to begin.";
+  clearColorButtons();
+  clearMatcherButton();
+  gameButton.textContent = "Start";
+});
+
 buttons.forEach((button, index) => {
   button.addEventListener("click", () => validateAnswer(index));
-  
 });
 
 gameButton.addEventListener("click", toggleGame);
