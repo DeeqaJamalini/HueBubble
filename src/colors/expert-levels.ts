727df31edf670ac18de80e4de2660ce94a5d15a6
DeeqaJamalini <deeqa.j11@gmail.com>
2023-12-11 16:15:31 +0000

    changed color for expert level 2 level 2

--- src/colors/expert-levels.ts
+++ src/colors/expert-levels.ts
@@ -24,23 +24,23 @@ export const expertLevel1: Level = {
 
 export const expertLevel2: Level = {
   matcherButton: {
-    "matcher-button": "#1C110A",
+    "matcher-button": "#062b30",
   },
 
   colorButtons: [
-    { color: "#1C110A", matcher: true },
-    { color: "#1C1100", matcher: false },
-    { color: "#1C1104", matcher: false },
-    { color: "#1C1108", matcher: false },
-    { color: "#1C110C", matcher: false },
-    { color: "#1C1110", matcher: false },
-    { color: "#1C1114", matcher: false },
-    { color: "#1C1118", matcher: false },
+    { color: "#062b30", matcher: true },
+    { color: "#093f46", matcher: false },
+    { color: "#094047", matcher: false },
+    { color: "#062a2f", matcher: false },
+    { color: "#0d2512", matcher: false },
+    { color: "#162709", matcher: false },
+    { color: "#1e0012", matcher: false },
+    { color: "#2b1900", matcher: false },
     { color: "#1C111C", matcher: false },
     { color: "#1C1120", matcher: false },
     { color: "#1C1124", matcher: false },
-    { color: "#1C1128", matcher: false },
-    { color: "#1C112C", matcher: false },
+    { color: "#402e41", matcher: false },
+    { color: "#0c5660", matcher: false },
   ],
 };
 
@@ -83,31 +83,28 @@ export const expertLevel4: Level = {
     { color: "#3fc08c", matcher: false },
     { color: "#3fc08e", matcher: false },
     { color: "#3fc08f", matcher: false },
-
-
   ],
 };
 
-
 export const expertLevel5: Level = {
   matcherButton: {
-    "matcher-button": "#f7f007",  
+    "matcher-button": "#f7f007",
   },
   colorButtons: [
-    { color: "#f7f00d", matcher: false },  
-    { color: "#f7f013", matcher: false },  
-    { color: "#f7f019", matcher: false },  
-    { color: "#f7f01f", matcher: false },  
-    { color: "#f7f025", matcher: false },  
-    { color: "#f7f02b", matcher: false },  
-    { color: "#f7f031", matcher: false },  
-    { color: "#f7f037", matcher: false },  
+    { color: "#f7f00d", matcher: false },
+    { color: "#f7f013", matcher: false },
+    { color: "#f7f019", matcher: false },
+    { color: "#f7f01f", matcher: false },
+    { color: "#f7f025", matcher: false },
+    { color: "#f7f02b", matcher: false },
+    { color: "#f7f031", matcher: false },
+    { color: "#f7f037", matcher: false },
 
-    { color: "#f7f043", matcher: false },  
-    { color: "#f7f049", matcher: false },  
-    { color: "#f7f04f", matcher: false },  
-    { color: "#f7f055", matcher: false },  
-    { color: "#f7f007", matcher: true },   
+    { color: "#f7f043", matcher: false },
+    { color: "#f7f049", matcher: false },
+    { color: "#f7f04f", matcher: false },
+    { color: "#f7f055", matcher: false },
+    { color: "#f7f007", matcher: true },
   ],
 };
 
@@ -132,17 +129,12 @@ export const expertLevel6: Level = {
   ],
 };
 
-
 export const expertLevel7: Level = {
   matcherButton: {
-
     "matcher-button": "#ad36c9",
-
   },
 
-
   colorButtons: [
-
     { color: "#ad36c9", matcher: true },
     { color: "#ad36cf", matcher: false },
     { color: "#ad36d5", matcher: false },
@@ -156,21 +148,15 @@ export const expertLevel7: Level = {
     { color: "#ad36c3", matcher: false },
     { color: "#ad36d3", matcher: false },
     { color: "#ad36e3", matcher: false },
-  ]
-
-  
-}
+  ],
+};
 
 export const expertLevel8: Level = {
   matcherButton: {
-
-
     "matcher-button": "#00c7ff",
   },
 
   colorButtons: [
-
-    
     { color: "#00c7f5", matcher: false },
     { color: "#00c7eb", matcher: false },
     { color: "#00c7e1", matcher: false },
@@ -184,201 +170,150 @@ export const expertLevel8: Level = {
     { color: "#00c79b", matcher: false },
     { color: "#00c791", matcher: false },
     { color: "#00c787", matcher: false },
-     
-
-
-
-  ]
-
-}
-
+  ],
+};
 
 export const expertLevel9: Level = {
   matcherButton: {
-
-    "matcher-button": "#ff0000",  
-
+    "matcher-button": "#ff0000",
   },
 
-  colorButtons: [ 
-      
-     
-      { color: "#ff0006", matcher: false },
-      { color: "#ff000c", matcher: false },
-      { color: "#ff0012", matcher: false },
-      { color: "#ff0018", matcher: false },
-      { color: "#ff001e", matcher: false },
-      { color: "#ff0024", matcher: false },
-      
-      { color: "#ff002a", matcher: false },
-      { color: "#ff0030", matcher: false },
-      { color: "#ff0036", matcher: false },
-      { color: "#ff003c", matcher: false },
-      { color: "#ff0042", matcher: false },
-      { color: "#ff0000", matcher: true },
-      { color: "#ff0048", matcher: false },
-    ]
+  colorButtons: [
+    { color: "#ff0006", matcher: false },
+    { color: "#ff000c", matcher: false },
+    { color: "#ff0012", matcher: false },
+    { color: "#ff0018", matcher: false },
+    { color: "#ff001e", matcher: false },
+    { color: "#ff0024", matcher: false },
 
-  
-
-}
-
+    { color: "#ff002a", matcher: false },
+    { color: "#ff0030", matcher: false },
+    { color: "#ff0036", matcher: false },
+    { color: "#ff003c", matcher: false },
+    { color: "#ff0042", matcher: false },
+    { color: "#ff0000", matcher: true },
+    { color: "#ff0048", matcher: false },
+  ],
+};
 
 export const expertLevel10: Level = {
-
   matcherButton: {
-      
-      "matcher-button": "#c03f77",  
-  
-    },
+    "matcher-button": "#c03f77",
+  },
 
-    colorButtons: [
-
-     
-      { color: "#c03f7d", matcher: false },
-      { color: "#c03f83", matcher: false },
-      { color: "#c03f89", matcher: false },
-      { color: "#c03f8f", matcher: false },
-      { color: "#c03f95", matcher: false },
-      { color: "#c03f77", matcher: true },
-      { color: "#c03f9b", matcher: false },
-      { color: "#c03fa1", matcher: false },
-      { color: "#c03fa7", matcher: false },
-      { color: "#c03fad", matcher: false },
-      { color: "#c03fb3", matcher: false },
-      { color: "#c03fb9", matcher: false },
-      { color: "#c03fbf", matcher: false },
-    ]
-  }
-
-
+  colorButtons: [
+    { color: "#c03f7d", matcher: false },
+    { color: "#c03f83", matcher: false },
+    { color: "#c03f89", matcher: false },
+    { color: "#c03f8f", matcher: false },
+    { color: "#c03f95", matcher: false },
+    { color: "#c03f77", matcher: true },
+    { color: "#c03f9b", matcher: false },
+    { color: "#c03fa1", matcher: false },
+    { color: "#c03fa7", matcher: false },
+    { color: "#c03fad", matcher: false },
+    { color: "#c03fb3", matcher: false },
+    { color: "#c03fb9", matcher: false },
+    { color: "#c03fbf", matcher: false },
+  ],
+};
 
 export const expertLevel11: Level = {
-  
-    matcherButton: {
-        
-        "matcher-button": "#8e926d",  
-    
-      },
-  
-      colorButtons: [
-                
+  matcherButton: {
+    "matcher-button": "#8e926d",
+  },
 
-        
-        { color: "#8e926e", matcher: false },
-        { color: "#8e926f", matcher: false },
-        { color: "#8e9270", matcher: false },
-        { color: "#8e9271", matcher: false },
-        { color: "#8e9272", matcher: false },
-        { color: "#8e9273", matcher: false },
-        { color: "#8e9274", matcher: false },
-        { color: "#8e9275", matcher: false },
-        { color: "#8e9276", matcher: false },
-        { color: "#8e9277", matcher: false },
-        { color: "#8e926d", matcher: true },
-        { color: "#8e9278", matcher: false },
-        { color: "#8e9279", matcher: false },
-
-
-      ]
-
-
-  
-      
-
-}
+  colorButtons: [
+    { color: "#8e926e", matcher: false },
+    { color: "#8e926f", matcher: false },
+    { color: "#8e9270", matcher: false },
+    { color: "#8e9271", matcher: false },
+    { color: "#8e9272", matcher: false },
+    { color: "#8e9273", matcher: false },
+    { color: "#8e9274", matcher: false },
+    { color: "#8e9275", matcher: false },
+    { color: "#8e9276", matcher: false },
+    { color: "#8e9277", matcher: false },
+    { color: "#8e926d", matcher: true },
+    { color: "#8e9278", matcher: false },
+    { color: "#8e9279", matcher: false },
+  ],
+};
 
 export const expertLevel12: Level = {
   matcherButton: {
-      
-      "matcher-button": "#b3f9ea",  
+    "matcher-button": "#b3f9ea",
+  },
 
-    },
+  colorButtons: [
+    { color: "#b3f9f0", matcher: false },
+    { color: "#b3f9f6", matcher: false },
+    { color: "#b3f9fc", matcher: false },
+    { color: "#b3f9e3", matcher: false },
+    { color: "#b3f9ea", matcher: true },
+    { color: "#b3f9e9", matcher: false },
+    { color: "#b3f9e6", matcher: false },
+    { color: "#b3f9e4", matcher: false },
+    { color: "#b3f9e1", matcher: false },
+    { color: "#b3f9e7", matcher: false },
+    { color: "#b3f9e5", matcher: false },
+    { color: "#b3f9e8", matcher: false },
+    { color: "#b3f9e9", matcher: false },
+  ],
+};
 
-    colorButtons: [ 
-        
-      { color: "#b3f9f0", matcher: false },
-      { color: "#b3f9f6", matcher: false },
-      { color: "#b3f9fc", matcher: false },
-      { color: "#b3f9e3", matcher: false },
-      { color: "#b3f9ea", matcher: true },
-      { color: "#b3f9e9", matcher: false },
-      { color: "#b3f9e6", matcher: false },
-      { color: "#b3f9e4", matcher: false },
-      { color: "#b3f9e1", matcher: false },
-      { color: "#b3f9e7", matcher: false },
-      { color: "#b3f9e5", matcher: false },
-      { color: "#b3f9e8", matcher: false },
-      { color: "#b3f9e9", matcher: false },
-      ]
-
-
-}
-
-export const expertLevel13: Level ={
+export const expertLevel13: Level = {
   matcherButton: {
-      
-      "matcher-button": "#4f7bb0",  
+    "matcher-button": "#4f7bb0",
+  },
 
-    },
+  colorButtons: [
+    { color: "#4f7bb6", matcher: false },
+    { color: "#4f7bbc", matcher: false },
+    { color: "#4f7bc2", matcher: false },
+    { color: "#4f7ba9", matcher: false },
 
-    colorButtons: [ 
-        
-      { color: "#4f7bb6", matcher: false },
-      { color: "#4f7bbc", matcher: false },
-      { color: "#4f7bc2", matcher: false },
-      { color: "#4f7ba9", matcher: false },
-     
-      { color: "#4f7bb1", matcher: false },
-      { color: "#4f7bae", matcher: false },
-      { color: "#4f7bac", matcher: false },
-      { color: "#4f7bb0", matcher: true },
-      { color: "#4f7ba9", matcher: false },
-      { color: "#4f7baf", matcher: false },
-      { color: "#4f7bad", matcher: false },
-      { color: "#4f7bb2", matcher: false },
-      { color: "#4f7bb3", matcher: false },
-    ]
-
-}
+    { color: "#4f7bb1", matcher: false },
+    { color: "#4f7bae", matcher: false },
+    { color: "#4f7bac", matcher: false },
+    { color: "#4f7bb0", matcher: true },
+    { color: "#4f7ba9", matcher: false },
+    { color: "#4f7baf", matcher: false },
+    { color: "#4f7bad", matcher: false },
+    { color: "#4f7bb2", matcher: false },
+    { color: "#4f7bb3", matcher: false },
+  ],
+};
 
 export const expertLevel14: Level = {
   matcherButton: {
-      
-      "matcher-button": "#e5b0e4",  
+    "matcher-button": "#e5b0e4",
+  },
 
-    },
+  colorButtons: [
+    { color: "#e5b0e4", matcher: true },
+    { color: "#e5b0ea", matcher: false },
+    { color: "#e5b0f0", matcher: false },
+    { color: "#e5b0f6", matcher: false },
+    { color: "#e5b0dc", matcher: false },
 
-    colorButtons: [ 
-      { color: "#e5b0e4", matcher: true },
-      { color: "#e5b0ea", matcher: false },
-      { color: "#e5b0f0", matcher: false },
-      { color: "#e5b0f6", matcher: false },
-      { color: "#e5b0dc", matcher: false },
-      
-      { color: "#e5b0e3", matcher: false },
-      { color: "#e5b0e1", matcher: false },
-      { color: "#e5b0df", matcher: false },
-      { color: "#e5b0dd", matcher: false },
-      { color: "#e5b0e2", matcher: false },
-      { color: "#e5b0e0", matcher: false },
-      { color: "#e5b0e5", matcher: false },
-      { color: "#e5b0e6", matcher: false },
-    
-    ]
-
-
-}
+    { color: "#e5b0e3", matcher: false },
+    { color: "#e5b0e1", matcher: false },
+    { color: "#e5b0df", matcher: false },
+    { color: "#e5b0dd", matcher: false },
+    { color: "#e5b0e2", matcher: false },
+    { color: "#e5b0e0", matcher: false },
+    { color: "#e5b0e5", matcher: false },
+    { color: "#e5b0e6", matcher: false },
+  ],
+};
 
 export const expertLevel15: Level = {
   matcherButton: {
-    "matcher-button":  "#292740"
-  }
-
-  ,
+    "matcher-button": "#292740",
+  },
 
   colorButtons: [
-    
     { color: "#292741", matcher: false },
     { color: "#292742", matcher: false },
     { color: "#292743", matcher: false },
@@ -392,17 +327,15 @@ export const expertLevel15: Level = {
     { color: "#29274a", matcher: false },
     { color: "#29274b", matcher: false },
     { color: "#29274c", matcher: false },
-  ]
-}
-
+  ],
+};
 
 export const expertLevel16: Level = {
   matcherButton: {
-    "matcher-button": "#946b6e" 
+    "matcher-button": "#946b6e",
   },
 
   colorButtons: [
-   
     { color: "#946b6f", matcher: false },
     { color: "#946b70", matcher: false },
     { color: "#946b71", matcher: false },
@@ -416,18 +349,15 @@ export const expertLevel16: Level = {
     { color: "#946b78", matcher: false },
     { color: "#946b79", matcher: false },
     { color: "#946b7a", matcher: false },
-  ]
-
-}
-
+  ],
+};
 
 export const expertLevel17: Level = {
   matcherButton: {
-    "matcher-button": "#b8c6d1" 
+    "matcher-button": "#b8c6d1",
   },
 
   colorButtons: [
-   
     { color: "#b8c6d2", matcher: false },
     { color: "#b8c6d3", matcher: false },
     { color: "#b8c6d4", matcher: false },
@@ -441,18 +371,15 @@ export const expertLevel17: Level = {
     { color: "#b8c6db", matcher: false },
     { color: "#b8c6dc", matcher: false },
     { color: "#b8c6dd", matcher: false },
-  ]
+  ],
+};
 
-}
-
-export const expertLevel18: Level = { 
-
+export const expertLevel18: Level = {
   matcherButton: {
-    "matcher-button": "#f7f007" 
+    "matcher-button": "#f7f007",
   },
 
   colorButtons: [
-   
     { color: "#f7f00d", matcher: false },
     { color: "#f7f013", matcher: false },
     { color: "#f7f019", matcher: false },
@@ -466,46 +393,40 @@ export const expertLevel18: Level = {
     { color: "#f7f049", matcher: false },
     { color: "#f7f04f", matcher: false },
     { color: "#f7f055", matcher: false },
-  ]
-
-}
+  ],
+};
 
 export const expertLevel19: Level = {
-
   matcherButton: {
-    "matcher-button": "#a18b5e"
+    "matcher-button": "#a18b5e",
   },
 
-
   colorButtons: [
-   
     { color: "#a18b5f", matcher: false },
     { color: "#a18b60", matcher: false },
     { color: "#a18b61", matcher: false },
     { color: "#a18b62", matcher: false },
     { color: "#a18b63", matcher: false },
-    
+
     { color: "#a18b5e", matcher: true },
     { color: "#a18b64", matcher: false },
-    
+
     { color: "#a18b65", matcher: false },
     { color: "#a18b66", matcher: false },
-    
+
     { color: "#a18b67", matcher: false },
     { color: "#a18b68", matcher: false },
     { color: "#a18b69", matcher: false },
-    { color: "#a18b6a", matcher: false },]
-  
-
-}
+    { color: "#a18b6a", matcher: false },
+  ],
+};
 
 export const expertLevel20: Level = {
   matcherButton: {
-    "matcher-button": "#bd424b"
+    "matcher-button": "#bd424b",
   },
 
   colorButtons: [
-         
     { color: "#bd424c", matcher: false },
     { color: "#bd424d", matcher: false },
     { color: "#bd424e", matcher: false },
@@ -514,15 +435,11 @@ export const expertLevel20: Level = {
     { color: "#bd4251", matcher: false },
     { color: "#bd4252", matcher: false },
     { color: "#bd4253", matcher: false },
-   
+
     { color: "#bd4254", matcher: false },
     { color: "#bd4255", matcher: false },
     { color: "#bd424b", matcher: true },
     { color: "#bd4256", matcher: false },
     { color: "#bd4257", matcher: false },
-  ]
-
-  }
-
-
-
+  ],
+};
